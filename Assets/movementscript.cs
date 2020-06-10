11198c848e51f4600f360b3e1f55f99359b170ef
LightStorm12345 <45642687+LightStorm12345@users.noreply.github.com>
2020-06-10 14:19:34 -0500

    Copied over files

---
+++ Assets/movementscript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementscript : MonoBehaviour
+{
+    public Transform trans;
+    private int currRot;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                trans.eulerAngles = new Vector3(0, 0, -45);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                trans.eulerAngles = new Vector3(0, 0, 45);
+            }
+            else
+            {
+                trans.eulerAngles = new Vector3(0, 0, 0);
+            }
+
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                trans.eulerAngles = new Vector3(0, 0, -135);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                trans.eulerAngles = new Vector3(0, 0, 135);
+            }
+            else
+            {
+                trans.eulerAngles = new Vector3(0, 0, 180);
+            }
+        }
+
+        else if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            trans.eulerAngles = new Vector3(0, 0, 90);
+        }
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            trans.eulerAngles = new Vector3(0, 0, -90);
+        }
+    }
+}
